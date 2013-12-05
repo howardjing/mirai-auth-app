95b3c3bed4f62e3adf347e6844436876b4737a01
Howard Jing <howard.jing@gmail.com>
2013-12-05 07:06:27 -0500

    dummy mirai-auth gem usage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiraiAuthApp::Application.config.secret_token = '798db6fdd4855f2b2a09bf6160592c6a9cec7099b6e02e5cd1b29f9db2fbb6e86da5922df030d9dd9ecaa913c2f7f551771563e28055756fcee86a4c895868da'
